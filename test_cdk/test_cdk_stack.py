f4ef7d06e764a3f2809df9f780bab743c83f0ced
Nick Bright <nickbright01@gmail.com>
2026-02-06 12:27:07 -0500

    Add test S3 bucket to CDK stack

--- test_cdk/test_cdk_stack.py
+++ test_cdk/test_cdk_stack.py
@@ -1,7 +1,6 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_s3 as s3,
 )
 from constructs import Construct
 
@@ -10,10 +9,12 @@ class TestCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "TestCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Create a simple S3 bucket
+        s3.Bucket(
+            self,
+            "TestBucket-cdk",
+            bucket_name="test-cdk-bucket-unique-12345",  # must be globally unique
+            versioned=True,
+            removal_policy=s3.RemovalPolicy.DESTROY,  # delete bucket when stack is destroyed
+            auto_delete_objects=True                  # deletes objects automatically when removing stack
+        )
